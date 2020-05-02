d672990c173a29a963620fd68932830ad2b36437
Raycon <rayconlimabatista@gmail.com>
2020-05-02 11:26:09 -0400

    Projeto básico de aprendizado ao mobx dentro do flutter.

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutterpraticamobx/pages/login_page.dart';
+import 'package:flutterpraticamobx/stores/login_store.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<LoginStore>(
+      create: (_) => LoginStore(),
+      child: MaterialApp(
+        title: 'MobX Tutorial',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.deepPurpleAccent,
+          cursorColor: Colors.deepPurpleAccent,
+          scaffoldBackgroundColor: Colors.deepPurpleAccent,
+        ),
+        home: LoginPage(),
+      ),
+    );
+  }
+}
